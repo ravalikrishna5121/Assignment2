27870612d86cc29969caa601f0ddbb9ae18318ca
ravalikrishna5121 <ravalikrishna1996@gmail.com>
2023-02-06 18:02:00 -0500

    first commit

---
+++ PROG2070Assign2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROG2070Assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
