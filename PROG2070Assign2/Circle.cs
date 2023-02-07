972ea7b41790844a40e16f0324fece7f05205504
ravalikrishna5121 <ravalikrishna1996@gmail.com>
2023-02-07 17:27:09 -0500

    Second commit

---
+++ PROG2070Assign2/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROG2070Assign2
+{
+    public class Circle
+    {
+        private int pvtradius { get; set; }
+        public int InputRadius;
+        public Circle() //default constructor
+        {
+            pvtradius = 1;
+        }
+
+        public Circle ( int InputRadius)
+        {
+            pvtradius= InputRadius;
+        }
+        public int GetRadius()
+        {
+            return pvtradius;
+        }
+
+        public void SetRadius(int inputRadius)
+        {
+            pvtradius = inputRadius;
+        }
+
+        public double GetCircumference()
+        {
+
+            return 2 * 3.14 * this.pvtradius;
+        }
+
+        public double GetArea()
+        {
+            return 3.14 * pvtradius * pvtradius;
+        }
+
+    }
+}
--- PROG2070Assign2/Program.cs
+++ PROG2070Assign2/Program.cs
@@ -6,7 +6,8 @@ namespace PROG2070Assign2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
-}
+    }
+
